a3e76421b564c240c7cb256b54b18c94bb52ccaf
Denisa <calota.denisa14@gmail.com>
2021-02-27 11:00:35 +0200

    Project structure

--- src/App.js
+++ src/App.js
@@ -4,18 +4,6 @@ import db from './firebase/FirebaseConfig';
 class App extends React.Component {
 
   render() {
-    db.collection("cities").doc("LA").set({
-      name: "Los Angeles",
-      state: "CA",
-      country: "USA"
-    })
-    .then(() => {
-        console.log("Document successfully written!");
-    })
-    .catch((error) => {
-        console.error("Error writing document: ", error);
-    });
-
     return (
       <div className="App">
       </div>
